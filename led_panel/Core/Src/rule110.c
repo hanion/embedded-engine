5f92f84e59430765052a3064717f25025ae02e9b
yigithanion <55713249+hanion@users.noreply.github.com>
2024-08-21 10:46:16 +0300

    better rule110

--- led_panel/Core/Src/rule110.c
+++ led_panel/Core/Src/rule110.c
@@ -17,40 +17,18 @@
 uint8_t rbuffer[WIDTH][HEIGHT] = {0};
 uint8_t rcurrent_row = 0;
 
-void on_ready() {
-	memset(rbuffer, 0, sizeof(rbuffer));
-	rbuffer[WIDTH-1][0] = 7;
-	//rbuffer[31][0] = 7;
-}
-void on_update() {
-	clear_back_buffer();
 
+void advance_rule() {
 	uint8_t new_row[WIDTH] = {0};
 
 	for (uint8_t i = 0; i < WIDTH; ++i) {
-		uint8_t a = 0;
-		uint8_t b = rbuffer[i][rcurrent_row];
-		uint8_t c = 0;
+		uint8_t a = (i > 0)       ? rbuffer[i-1][rcurrent_row] : 0;
+		uint8_t b =                 rbuffer[i  ][rcurrent_row]    ;
+		uint8_t c = (i+1 < WIDTH) ? rbuffer[i+1][rcurrent_row] : 0;
 
-		if (i > 0) {
-			a = rbuffer[i-1][rcurrent_row];
-		}
-		if (i+1 < WIDTH) {
-			c = rbuffer[i+1][rcurrent_row];
-		}
-
-		uint8_t result = 0;
-
-		if (( a &&  b && !c )||
-			( a && !b &&  c )||
-			(!a &&  b &&  c )||
-			(!a &&  b && !c )||
-			(!a && !b &&  c )
-			) {
-			result = 7;
-		}
-
-		new_row[i] = result;
+		uint8_t pattern = (a << 2) | (b << 1) | c;
+		uint8_t rule = 110;
+		new_row[i] = (rule >> pattern) & 1;
 	}
 
 	// push new_row
@@ -72,9 +50,46 @@ void on_update() {
 		}
 	}
 
+}
+void copy_to_back_buffer() {
+	//static_assert(sizeof((*back_buffer)) == sizeof(rbuffer));
+	//memcpy((*back_buffer), rbuffer, sizeof(rbuffer));
 
-	static_assert(sizeof((*back_buffer)) == sizeof(rbuffer));
-	memcpy((*back_buffer), rbuffer, sizeof(rbuffer));
+	for (uint8_t x = 0; x < WIDTH; ++x) {
+		for (uint8_t y = 0; y < HEIGHT; ++y) {
+			set_pixel_w(x, y, rbuffer[x][y]);
+		}
+	}
+}
+
+
+
+void on_ready() {
+	memset(rbuffer, 0, sizeof(rbuffer));
+	rbuffer[WIDTH-1][0] = 1;
+	//rbuffer[31][0] = 1;
+}
+
+void on_update() {
+	clear_back_buffer();
+
+	advance_rule();
+
+	copy_to_back_buffer();
+
+//	// for fun
+//	if (rbuffer[0][HEIGHT-1]) {
+//		// scroll buffer up
+//		for (uint8_t x = 0; x < WIDTH; ++x) {
+//			for (uint8_t y = 0; y < (HEIGHT-1); ++y) {
+//				rbuffer[x][y] = rbuffer[x][y+1];
+//			}
+//		}
+//		for (int i = 0; i < WIDTH; ++i) {
+//			rbuffer[i][HEIGHT-1] = 0;
+//		}
+//		rbuffer[WIDTH-1][HEIGHT-1] = 1;
+//	}
 }
 
 
