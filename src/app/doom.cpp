c6810605f00ae27959bfc353325344d0422f3606
yigithanion <55713249+hanion@users.noreply.github.com>
2024-09-04 11:37:37 +0300

    enemies and projectiles

--- src/app/doom.cpp
+++ src/app/doom.cpp
@@ -3,19 +3,20 @@
 #include "math.hpp"
 #include "renderer.hpp"
 #include <cmath>
+#include <cstring>
 
 #define RAY_MAX_DISTANCE 100
+#define ENEMY_COLLIDER_RADIUS 0.4f
 
 #define MAP_WIDTH 16
 #define MAP_HEIGHT 16
-
-uint8_t map[MAP_HEIGHT][MAP_WIDTH] = {
+const uint8_t map[MAP_HEIGHT][MAP_WIDTH] = {
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 	{1, 0, 1, 4, 2, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1},
 	{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
-	{1, 0, 1, 0, 1, 1, 1, 1, 4, 4, 0, 0, 1, 0, 0, 1},
-	{1, 0, 1, 0, 1, 0, 0, 0, 4, 4, 0, 0, 1, 0, 0, 1},
+	{1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1},
+	{1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1},
 	{1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1},
 	{1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 2, 2, 1, 0, 0, 1},
 	{1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0, 0, 1},
@@ -31,24 +32,46 @@ float depth_buffer[WIDTH];
 
 #define TEXTURE_WIDTH 16
 #define TEXTURE_HEIGHT 16
-uint8_t circle_texture[TEXTURE_HEIGHT][TEXTURE_WIDTH] = {
-	{0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0},
-	{0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0},
-	{0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0},
-	{0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0},
-	{0, 5, 5, 5, 5, 8, 8, 5, 5, 8, 8, 5, 5, 5, 5, 0},
-	{5, 5, 5, 5, 5, 8, 8, 5, 5, 8, 8, 5, 5, 5, 5, 5},
-	{5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
-	{5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
-	{5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
-	{5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
-	{5, 5, 5, 5, 5, 8, 8, 8, 8, 8, 8, 5, 5, 5, 5, 5},
-	{0, 5, 5, 5, 5, 8, 8, 8, 8, 8, 8, 5, 5, 5, 5, 0},
-	{0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0},
-	{0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0},
-	{0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0},
-	{0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0}
+const uint8_t circle_texture[TEXTURE_HEIGHT][TEXTURE_WIDTH] = {
+	{0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 6, 0, 0, 0, 0, 0},
+	{0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0},
+	{0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 0},
+	{0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0},
+	{0, 6, 6, 6, 6, 8, 8, 6, 6, 8, 8, 6, 6, 6, 6, 0},
+	{6, 6, 6, 6, 6, 8, 8, 6, 6, 8, 8, 6, 6, 6, 6, 6},
+	{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+	{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+	{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+	{6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+	{6, 6, 6, 6, 6, 8, 8, 8, 8, 8, 8, 6, 6, 6, 6, 6},
+	{0, 6, 6, 6, 6, 8, 8, 8, 8, 8, 8, 6, 6, 6, 6, 0},
+	{0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0},
+	{0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 0},
+	{0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0},
+	{0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 6, 0, 0, 0, 0, 0}
 };
+
+#define PROJECTILE_TEXTURE_WIDTH 9
+#define PROJECTILE_TEXTURE_HEIGHT 9
+const uint8_t projectile_circle_texture[PROJECTILE_TEXTURE_HEIGHT][PROJECTILE_TEXTURE_WIDTH] = {
+	{0, 0, 0, 7, 7, 7, 0, 0, 0},
+	{0, 7, 7, 4, 4, 4, 7, 7, 0},
+	{0, 7, 4, 4, 4, 4, 4, 7, 0},
+	{7, 4, 4, 4, 4, 4, 4, 4, 7},
+	{7, 4, 4, 4, 4, 4, 4, 4, 7},
+	{7, 4, 4, 4, 4, 4, 4, 4, 7},
+	{0, 7, 4, 4, 4, 4, 4, 7, 0},
+	{0, 7, 7, 4, 4, 4, 7, 7, 0},
+	{0, 0, 0, 7, 7, 7, 0, 0, 0}
+};
+Texture projectile_texture = {
+	.data = &projectile_circle_texture[0][0],
+	.width = PROJECTILE_TEXTURE_WIDTH,
+	.height = PROJECTILE_TEXTURE_HEIGHT,
+	.scale = 0.2f
+};
+
+
 
 struct raycast_result {
 	bool successful = false;
@@ -131,7 +154,7 @@ void draw_filled(int x, float line_height, const Color& color) {
 }
 
 
-void Doom::draw_sprite(vf2 sprite) {
+void Doom::draw_sprite(vf2 sprite, const Texture& texture) {
 	vf2 relative_position = sprite - player;
 
 	vf2 transformed_position = {
@@ -144,24 +167,24 @@ void Doom::draw_sprite(vf2 sprite) {
 	}
 
 	float screen_x = (WIDTH / 2.0f) * (1 - transformed_position.x / transformed_position.y);
-	float sprite_size = HEIGHT / transformed_position.y;
+	float sprite_size = (HEIGHT / transformed_position.y) * texture.scale;
 
 	int sprite_left = static_cast<int>(screen_x - sprite_size / 2);
 	int sprite_right = static_cast<int>(screen_x + sprite_size / 2);
 
 	for (int x = sprite_left; x < sprite_right; ++x) {
 		if (x >= 0 && x < WIDTH) {
-			float tex_x = (x - sprite_left) / sprite_size * TEXTURE_WIDTH;
-			tex_x = std::min(std::max(tex_x, 0.0f), static_cast<float>(TEXTURE_WIDTH - 1));
+			float tex_x = (x - sprite_left) / sprite_size * texture.width;
+			tex_x = std::min(std::max(tex_x, 0.0f), static_cast<float>(texture.width - 1));
 
 			if (transformed_position.y < depth_buffer[x]) {
 				for (int y = 0; y < sprite_size; ++y) {
 					int screen_y = static_cast<int>(HEIGHT / 2.0f - sprite_size / 2 + y);
 					if (screen_y >= 0 && screen_y < HEIGHT) {
-						float tex_y = y / sprite_size * TEXTURE_HEIGHT;
-						tex_y = std::min(std::max(tex_y, 0.0f), static_cast<float>(TEXTURE_HEIGHT - 1));
+						float tex_y = y / sprite_size * texture.height;
+						tex_y = std::min(std::max(tex_y, 0.0f), static_cast<float>(texture.height - 1));
 
-						uint8_t tex_color = circle_texture[static_cast<int>(tex_y)][static_cast<int>(tex_x)];
+						uint8_t tex_color = texture.data[static_cast<int>(tex_y) * texture.width + static_cast<int>(tex_x)];
 						if (tex_color) {
 							Color color;
 							color.value = tex_color;
@@ -174,8 +197,31 @@ void Doom::draw_sprite(vf2 sprite) {
 	}
 }
 
+void Doom::on_ready() {
+	std::vector<vf2> enemy_positions = {
+		{4.2f, 2.2f},
+		{8.5f, 7.5f},
+		{9.5f, 7.5f},
+		{10.5f, 2.5f},
+		{10.5f, 4.5f},
+	};
+	Enemy enemy = {
+		.position = {8.2f, 8.2f},
+		.texture = {
+			.data = &circle_texture[0][0],
+			.width = TEXTURE_WIDTH,
+			.height = TEXTURE_HEIGHT,
+		}
+	};
+	for (const auto& pos : enemy_positions) {
+		enemy.position = pos;
+		m_enemies.push_back(enemy);
+	}
+	m_enemies.push_back(enemy);
+}
 
 void Doom::on_update(double delta_time) {
+	m_delta_time = delta_time;
 
 	Renderer::clear_back_buffer();
 
@@ -196,15 +242,37 @@ void Doom::on_update(double delta_time) {
 			draw_filled(ray, line_height, color);
 		}
 	}
-	draw_sprite(test_sprite);
-// 	Renderer::draw_number(m_view_degree, 0,0);
-// 	Renderer::draw_number(m_wall_height, 0,8);
+
+	update_enemies(delta_time);
+
+	// draw enemies
+	for (auto& enemy : m_enemies) {
+		draw_sprite(enemy.position, enemy.texture);
+	}
+
+	// update projectiles
+	update_projectiles(delta_time);
+
+	// draw projectiles
+	for (auto& proj : m_projectiles) {
+		draw_sprite(proj.position, proj.texture);
+	}
 }
 
-void Doom::on_ready() {}
 
 
-bool collision(vf2 pos) {
+void Doom::shoot() {
+	Projectile proj = {
+		.position = player,
+		.direction = player_dir,
+		.speed = 10.0f,
+		.texture = projectile_texture
+	};
+	m_projectiles.push_back(proj);
+}
+
+
+bool Doom::collision(vf2 pos) {
 	if (pos.x < 0 || pos.x >= MAP_WIDTH || pos.y < 0 || pos.y >= MAP_HEIGHT) {
 		return true;
 	}
@@ -216,32 +284,108 @@ bool collision(vf2 pos) {
 	return false;
 }
 
-void Doom::on_event(Event event) {
-	if (event.type == Event::Type::Released) {
+bool Doom::collide_with_enemy(vf2 pos) {
+	for (auto it = m_enemies.begin(); it != m_enemies.end();) {
+		auto& enemy = *it;
+
+		float dx = std::abs(pos.x - enemy.position.x);
+		float dy = std::abs(pos.y - enemy.position.y);
+		if (dy < ENEMY_COLLIDER_RADIUS && dx < ENEMY_COLLIDER_RADIUS) {
+			enemy.health -= 50.0f;
+			if (enemy.health <= 0) {
+				m_enemies.erase(it);
+			}
+			return true;
+		} else {
+			++it;
+		}
+	}
+	return false;
+}
+
+void Doom::update_projectiles(double delta_time) {
+	for (auto it = m_projectiles.begin(); it != m_projectiles.end();) {
+		auto& p = *it;
+		p.position += p.direction * p.speed * delta_time;
+
+		if (p.position.x < 0 || p.position.y < 0 ||
+			p.position.x > MAP_WIDTH || p.position.y > MAP_HEIGHT ||
+			collision(p.position) ||
+			collide_with_enemy(p.position) ) {
+			//++it;
+			it = m_projectiles.erase(it);
+		} else {
+			++it;
+		}
+	}
+}
+
+void Doom::update_enemies(double delta_time) {
+	for (auto& enemy : m_enemies) {
+		vf2 dif = player - enemy.position;
+		float distance = sqrt(dif.x * dif.x + dif.y * dif.y);
+
+		if (distance < ENEMY_COLLIDER_RADIUS) {
+			continue;
+		}
+
+		vf2 direction = { dif.x / distance, dif.y / distance };
+		move_and_slide(enemy.position, direction * enemy.speed * delta_time);
+
+	}
+}
+
+void Doom::move_and_slide(vf2& source, const vf2& delta) {
+	if (delta.x == 0.0f && delta.y == 0.0f) {
 		return;
 	}
 
-	vf2 new_pos = player;
+	vf2 new_pos = source + delta;
+	if (new_pos != source) {
+		if (!collision(new_pos)) {
+			source = new_pos;
+		} else if (!collision({new_pos.x, source.y})) {
+			source.x = new_pos.x;
+		} else if (!collision({source.x, new_pos.y})) {
+			source.y = new_pos.y;
+		}
+	}
+}
+
+
+
+void Doom::on_event(Event event) {
+	if (event.type == Event::Type::Released) {
+		return;
+	} else if (event.type == Event::Type::Pressed) {
+		switch (event.keycode) {
+			case ' ':
+				shoot();
+			break;
+		}
+	}
+
+	vf2 movement = {0.0f,0.0f};
 	switch (event.keycode) {
 		case 'w':
-			new_pos += player_dir * m_speed;
+			movement += player_dir * m_speed * m_delta_time;
 			break;
 		case 's':
-			new_pos -= player_dir * m_speed;
+			movement -= player_dir * m_speed * m_delta_time;
 			break;
 		case 'a':
-			new_pos.x += player_dir.y * m_speed;
-			new_pos.y -= player_dir.x * m_speed;
+			movement.x += player_dir.y * m_speed * m_delta_time;
+			movement.y -= player_dir.x * m_speed * m_delta_time;
 			break;
 		case 'd':
-			new_pos.x -= player_dir.y * m_speed;
-			new_pos.y += player_dir.x * m_speed;
+			movement.x -= player_dir.y * m_speed * m_delta_time;
+			movement.y += player_dir.x * m_speed * m_delta_time;
 			break;
 		case 'j':
-			player_dir = player_dir.rotated_z(-m_rotation_speed);
+			player_dir = player_dir.rotated_z(-m_rotation_speed * m_delta_time);
 			break;
 		case 'l':
-			player_dir = player_dir.rotated_z(m_rotation_speed);
+			player_dir = player_dir.rotated_z(m_rotation_speed * m_delta_time);
 			break;
 		case 'x':
 			m_view_degree += 0.1f;
@@ -258,15 +402,5 @@ void Doom::on_event(Event event) {
 		default:
 			break;
 	}
-
-	if (new_pos != player) {
-		if (!collision(new_pos)) {
-			player = new_pos;
-		} else if (!collision({new_pos.x, player.y})) {
-			player.x = new_pos.x;
-		} else if (!collision({player.x, new_pos.y})) {
-			player.y = new_pos.y;
-		}
-	}
-	
+	move_and_slide(player, movement);
 }
--- src/app/doom.hpp
+++ src/app/doom.hpp
@@ -5,6 +5,32 @@
 #include "vector.hpp"
 
 
+struct Texture {
+	const uint8_t *data = nullptr;
+	uint8_t width = 0;
+	uint8_t height = 0;
+	float scale = 1.0f;
+};
+
+struct Enemy {
+	vf2 position = { 0.0f, 0.0f };
+	float health = 100.0f;
+	float speed = 1.0f;
+	float fire_interval = 2.0f;
+
+	Texture texture;
+};
+
+struct Projectile {
+	vf2 position = { 0.0f, 0.0f };
+	vf2 direction;
+	float speed;
+	Texture texture;
+};
+
+
+
+
 class Doom : public Application {
 public:
 	Doom() : Application("Doom") {}
@@ -15,11 +41,21 @@ public:
 	virtual void on_event(Event event) override final;
 
 private:
-	void draw_sprite(vf2 sprite);
+	void draw_sprite(vf2 sprite, const Texture& texture);
+	void shoot();
+	void update_projectiles(double delta_time);
+	void update_enemies(double delta_time);
+
+	bool collision(vf2 pos);
+	bool collide_with_enemy(vf2 pos);
+
+	void move_and_slide(vf2& source, const vf2& delta);
 
 private:
-	float m_speed = 0.02f;
-	float m_rotation_speed = 0.8f;
+	float m_delta_time = 0.1f;
+
+	float m_speed = 2.0f;
+	float m_rotation_speed = 100.0f;
 
 	vf2 player = { 8.4f, 10.6f };
 	vf2 player_dir = { 1.0f, -1.0f };
@@ -27,7 +63,8 @@ private:
 	float m_view_degree = 88.0f;
 	float m_wall_height = 24.0f;
 
-	vf2 test_sprite = { 8.2f, 8.2f };
+	std::vector<Projectile> m_projectiles;
+	std::vector<Enemy> m_enemies;
 };
 
 
